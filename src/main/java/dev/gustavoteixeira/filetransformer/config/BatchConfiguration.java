ae72530e02a3f6764f267174410c2d4807c33203
Gustavo Teixeira <iwhrim@gmail.com>
2021-10-28 20:43:09 -0300

    THIS ONE WORKED BUT DID NOT EXIT WITH 0 OR 1

--- src/main/java/dev/gustavoteixeira/filetransformer/config/BatchConfiguration.java
+++ src/main/java/dev/gustavoteixeira/filetransformer/config/BatchConfiguration.java
@@ -1,18 +1,21 @@
 package dev.gustavoteixeira.filetransformer.config;
 
 
+import dev.gustavoteixeira.filetransformer.domain.Person;
+import dev.gustavoteixeira.filetransformer.mapper.fieldset.PersonFieldSetMapper;
+import dev.gustavoteixeira.filetransformer.processor.PersonItemProcessor;
+import dev.gustavoteixeira.filetransformer.writer.CustomItemWriter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.*;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemStreamReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -29,12 +32,12 @@ public class BatchConfiguration extends DefaultBatchConfigurer {
 
     @Override
     public void setDataSource(DataSource dataSource) {
-        super.setDataSource(null);
+        super.setDataSource(dataSource);
     }
 
     //
-//    @Autowired
-//    private ResourceLoader resourceLoader;
+    @Autowired
+    private DataSource dataSource;
     @Autowired
     private ResourceLoader resourceLoader;
     @Autowired
@@ -42,70 +45,80 @@ public class BatchConfiguration extends DefaultBatchConfigurer {
     @Autowired
     private StepBuilderFactory stepBuilderFactory;
 
-//    @Bean
-//    @StepScope
-//    public ItemStreamReader<Person> reader(@Value("#{jobParameters['bucketName']}") String bucketName,
-//                                           @Value("#{jobParameters['objectName']}") String objectName) {
-//        log.info("THE LOG I WANT TO SEE");
-//        log.info("bucketName: {}", bucketName);
-//        log.info("objectName: {}", objectName);
-//
-//        if (!objectName.matches("[a-z]+:.*")) {
-//            objectName = "file:" + objectName;
-//        }
-//        log.info("BEFORE THING THAT IS GOING TO BREAK FOR SURE");
-//        return new FlatFileItemReaderBuilder<Person>()
-//                .name("reader")
-//                .resource(resourceLoader.getResource(objectName))
-//                .delimited()
-//                .names("firstName", "lastName")
-//                .fieldSetMapper(new PersonFieldSetMapper())
-//                .build();
-//    }
-//
-//    @Bean
-//    public ItemProcessor<Person, Person> processor() {
-//        return new PersonItemProcessor();
-//    }
-//
-//    @Bean
-//    public ItemWriter<Person> writer() {
-//        return new CustomItemWriter();
-//    }
-//
-//    @Bean
-//    public Job ingestJob() {
-//        return jobBuilderFactory.get("ingestJob")
-//                .incrementer(new RunIdIncrementer())
-//                .start(step1())
-//                .build();
-//    }
-//
-//    @Bean
-//    public Step step1() {
-//        return stepBuilderFactory.get("ingest")
-//                .<Person, Person>chunk(10)
-//                .reader(reader(null, null))
-//                .processor(processor())
-//                .writer(writer())
-//                .build();
-//    }
+    @Bean
+    @StepScope
+    public ItemStreamReader<Person> reader(@Value("#{jobParameters['bucketName']}") String bucketName,
+                                           @Value("#{jobParameters['objectName']}") String objectName) {
+        log.info("THE LOG I WANT TO SEE");
+        log.info("THE LOG I WANT TO SEE");
+        log.info("THE LOG I WANT TO SEE");
+        log.info("THE LOG I WANT TO SEE");
+        log.info("THE LOG I WANT TO SEE");
+        log.info("THE LOG I WANT TO SEE");
+        log.info("THE LOG I WANT TO SEE");
+        log.info("THE LOG I WANT TO SEE");
+        log.info("THE LOG I WANT TO SEE");
+        log.info("THE LOG I WANT TO SEE");
+        log.info("THE LOG I WANT TO SEE");
+        log.info("THE LOG I WANT TO SEE");
+        log.info("THE LOG I WANT TO SEE");
+        log.info("bucketName: {}", bucketName);
+        log.info("objectName: {}", objectName);
+
+
+        log.info("BEFORE THING THAT IS GOING TO BREAK FOR SURE");
+        return new FlatFileItemReaderBuilder<Person>()
+                .name("reader")
+                .resource(resourceLoader.getResource(objectName))
+                .delimited()
+                .names("firstName", "lastName")
+                .fieldSetMapper(new PersonFieldSetMapper())
+                .build();
+    }
+
+    @Bean
+    public ItemProcessor<Person, Person> processor() {
+        return new PersonItemProcessor();
+    }
+
+    @Bean
+    public ItemWriter<Person> writer() {
+        return new CustomItemWriter();
+    }
+
+    @Bean
+    public Job ingestJob() {
+        return jobBuilderFactory.get("ingestJob")
+                .incrementer(new RunIdIncrementer())
+                .start(step1())
+                .build();
+    }
 
     @Bean
     public Step step1() {
-        return stepBuilderFactory.get("step1")
-                .tasklet((StepContribution contribution, ChunkContext chunkContext) -> RepeatStatus.FINISHED)
+        return stepBuilderFactory.get("ingest")
+                .<Person, Person>chunk(10)
+                .reader(reader(null, null))
+                .processor(processor())
+                .writer(writer())
                 .build();
     }
 
-    @Bean
-    public Job job(Step step1) throws Exception {
-        log.info("WUBBA LUBBA DUB DUB");
-        return jobBuilderFactory.get("job1")
-                .incrementer(new RunIdIncrementer())
-                .start(step1)
-                .build();
-    }
+//    @Bean
+//    public Step step1() {
+//        return stepBuilderFactory.get("step1")
+//                .tasklet((StepContribution contribution, ChunkContext chunkContext) -> RepeatStatus.FINISHED)
+//                .build();
+//    }
+//
+//    @Bean
+//    public Job job(Step step1) throws Exception {
+//        log.info("WUBBA LUBBA DUB DUB");
+//        return jobBuilderFactory.get("job1")
+//                .incrementer(new RunIdIncrementer())
+//                .start(step1)
+//                .build();
+//    }
 
 }
 
