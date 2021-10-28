28ca2a9dd85bf7d50bd427bf64ab70e311ba2770
Gustavo Teixeira <iwhrim@gmail.com>
2021-10-28 19:44:02 -0300

    before formatting pc

--- src/main/java/dev/gustavoteixeira/filetransformer/FiletransformerApplication.java
+++ src/main/java/dev/gustavoteixeira/filetransformer/FiletransformerApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.task.configuration.EnableTask;
 
 @SpringBootApplication(exclude = {BatchAutoConfiguration.class, DataSourceAutoConfiguration.class})
-@EnableTask
+//@EnableTask
 public class FiletransformerApplication {
 
     public static void main(String[] args) {
