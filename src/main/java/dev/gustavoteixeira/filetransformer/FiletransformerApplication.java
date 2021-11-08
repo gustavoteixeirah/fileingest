021e87985a3d221b1b40662e4ddeb1e3b6e543dd
Gustavo Teixeira <iwhrim@gmail.com>
2021-11-08 16:15:13 -0300

    added config

--- src/main/java/dev/gustavoteixeira/filetransformer/FiletransformerApplication.java
+++ src/main/java/dev/gustavoteixeira/filetransformer/FiletransformerApplication.java
@@ -2,16 +2,11 @@ package dev.gustavoteixeira.filetransformer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration;
-import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
-import org.springframework.cloud.aws.autoconfigure.messaging.MessagingAutoConfiguration;
 
 //@SpringBootApplication(exclude = {BatchAutoConfiguration.class, DataSourceAutoConfiguration.class})
 ////@EnableTask
 //@EnableTask
-@SpringBootApplication(exclude = {ContextRegionProviderAutoConfiguration.class,
-        ContextStackAutoConfiguration.class,
-        MessagingAutoConfiguration.class})
+@SpringBootApplication
 public class FiletransformerApplication {
 
     public static void main(String[] args) {
